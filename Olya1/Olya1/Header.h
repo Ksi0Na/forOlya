3585e7b59ff7bb5177912c2d6d8b707fd6d6b308
Toxa <p07a1330@ya.ru>
2019-11-03 19:07:35 +0300

    without S-errrors

--- Olya1/Olya1/Header.h
+++ Olya1/Olya1/Header.h
@@ -2,23 +2,23 @@
 #ifndef header
 #define header
 
-void questions_(unsigned);
-void view_info_(unsigned);
-void decripter_(unsigned);
-void decript_gender_(unsigned);
-void decript_age_(unsigned);
-void decript_education_(unsigned);
-void decript_hair_color_(unsigned);
-void decript_color_eyes_(unsigned);
-void decript_state_of_life_(unsigned);
-void decript_state_of_family_(unsigned);
-void gender_(unsigned);
-void age_(unsigned);
-void education_(unsigned);
-void hair_color_(unsigned);
-void color_eyes_(unsigned);
-void state_of_life_(unsigned);
-void leave_info_(unsigned);
-void state_of_family_(unsigned);
+static void questions_(void*);
+static void view_info_(unsigned);
+static void decripter_(unsigned);
+static void decript_gender_(unsigned);
+static void decript_age_(unsigned);
+static void decript_education_(unsigned);
+static void decript_hair_color_(unsigned);
+static void decript_color_eyes_(unsigned);
+static void decript_state_of_life_(unsigned);
+static void decript_state_of_family_(unsigned);
+static void gender_(unsigned);
+static void age_(unsigned);
+static void education_(unsigned);
+static void hair_color_(unsigned);
+static void color_eyes_(unsigned);
+static void state_of_life_(unsigned);
+static void leave_info_(unsigned);
+static void state_of_family_(unsigned);
 
 #endif header
